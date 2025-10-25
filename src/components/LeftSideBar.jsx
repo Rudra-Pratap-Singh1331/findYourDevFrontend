03c6367a3be6ca18b57a35012af14a46a592820e
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-10-25 13:34:04 +0530

    feat:chat notification live

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,31 +1,60 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { addFriends } from "../store/friendSlice";
 import { AVATAR_DEFAULT_URL } from "../constant/constant";
 import { addChat } from "../store/chatSlice";
+import { createSocket } from "../constant/socketConnection";
 
 const LeftSideBar = () => {
+  const [friends, setFriends] = useState(null);
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const friendList = useSelector((store) => store.friendList);
-  console.log(friendList);
   const fetchFreindList = async () => {
     try {
       const result = await axios.get("http://localhost:1001/user/friends", {
         withCredentials: true,
       });
-      dispatch(addFriends(result?.data?.friends));
+      const finalresult = result?.data?.friends?.map((frnds) => {
+        return { ...frnds, messageStatus: false };
+      });
+      setFriends(finalresult);
+      console.log(finalresult);
+      dispatch(addFriends(finalresult));
     } catch (error) {
-      if (error.response.status === 401) {
+      if (error?.response?.status === 401) {
         toast.error("Unauthorized!");
         navigate("/login");
       }
     }
   };
 
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    const { socket, disconnect } = createSocket();
+
+    socketRef.current = socket;
+
+    socketRef.current.emit("joinNotificationService", { userId });
+
+    socketRef.current.on("incomingMessage", ({ userId }) => {
+      setFriends((prevFriends) =>
+        prevFriends.map((f) =>
+          f._id === userId ? { ...f, messageStatus: true } : f
+        )
+      );
+    });
+
+    return () => {
+      disconnect();
+    };
+  }, [userId]); //ye isliye kyuni first store empty n thats why
   useEffect(() => {
     if (!friendList) {
       fetchFreindList();
@@ -41,12 +70,19 @@ const LeftSideBar = () => {
 
       {/* Friend List */}
       <div className="flex-1 overflow-y-auto p-4 space-y-3">
-        {friendList?.map((f) => (
+        {friends?.map((f) => (
           <div
             key={f._id}
-            className="flex items-center gap-3 p-2 rounded-md hover:bg-[#2d2d30] cursor-pointer transition"
+            className="flex items-center gap-3 p-2 rounded-md hover:bg-[#2d2d30] cursor-pointer transition relative" // relative added
             onClick={() => {
               dispatch(addChat(f));
+              setFriends((prev) =>
+                prev.map((friend) =>
+                  f._id === friend._id
+                    ? { ...friend, messageStatus: false }
+                    : friend
+                )
+              );
             }}
           >
             <div className="avatar">
@@ -54,7 +90,13 @@ const LeftSideBar = () => {
                 <img src={f.photoUrl || AVATAR_DEFAULT_URL} alt={f.name} />
               </div>
             </div>
-            <span className="font-medium text-[#d4d4d4]">{f.fullName}</span>
+
+            <span className="font-medium text-[#d4d4d4] relative">
+              {f.fullName}
+              {f.messageStatus && (
+                <span className="absolute top-0 right-[-10px] w-3 h-3 bg-blue-500 rounded-full animate-ping"></span>
+              )}
+            </span>
           </div>
         ))}
       </div>
