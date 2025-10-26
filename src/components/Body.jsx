edbddc1186da25cc5ff7bcdaaa947aeabaf4334c
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-10-26 11:42:05 +0530

    build:global fetching of data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -56,7 +56,7 @@ const Body = () => {
 
         <Outlet />
         {/* Right Sidebar */}
-        <RightSideBar requests={requests}  />
+        <RightSideBar requests={requests} />
       </div>
     </>
   );
